13065f00e31b8a4a8dd8538f1f00f9ba0e9437c4
Tiwaripranjali <148204311+Tiwaripranjali@users.noreply.github.com>
2023-10-30 19:02:44 +0530

    Create func.cpp

---
+++ func.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float a,b,c,d:
+  int a1,b1,c1;
+  float sum=0;
+  float avg;
+  void avg(float a,float b,float c,float d){
+    cout<<"enter 4 numbers:";
+    cin>>a>>b>>c>>d;
+    sum=a+b+c+d;
+    avg=sum/4
+    cout<<"average of 4 numbers:"<<avg  
+  }
+   void avg(int a1,int b1,int c1{
+    cout<<"enter 3 numbers:";
+    cin>>a1>>b1>>c1>>d1;
+    sum=a1+b1+c1+d1;
+    avg=sum/3
+    cout<<"average of 3 numbers:"<<avg  
+  }
+  avg(10.0,37.90,45,5.8);
+  avg(1,7,8);
+  return 0;
+}
