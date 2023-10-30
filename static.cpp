27aceffd1f603806faa9c56632c5956cd7845af4
Tiwaripranjali <148204311+Tiwaripranjali@users.noreply.github.com>
2023-10-30 18:59:10 +0530

    Create static.cpp

---
+++ static.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+double n1,n2,n3,n4,n5;
+double sum=0;
+double avg;
+cout<<"Enter five numbers:";
+cin>>n12>>n2>>n3>>n4>>n5;
+avg=sum/5;
+cout<<"Average:"<<avg;
+}
